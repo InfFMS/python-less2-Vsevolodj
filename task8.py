5e624c5e2d299918b7c491e61283650c5fea0f0b
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2024-10-07 22:21:26 +0700

    Cделал задачу 8

--- task8.py
+++ task8.py
@@ -2,10 +2,9 @@ c1=int(input())
 r1=int(input())
 c2=int(input())
 r2=int(input())
-
 if (r1>8 or r1<1 or r2>8 or r2<1):
     print("Неверно")
-elif (r1-c1-r2+c1==0 or r1+c1-r2-c1==0) and r1!=r2:
+elif (abs(c1-c2)==2 and abs(r1-r2)==1) or (abs(c1-c2)==1 and abs(r1-r2)==2):
     print("YES")
 else:
     print("NO")

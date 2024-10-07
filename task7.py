e17373a2064f58555f0e52a6b9535ccaae201147
Vsevolod <v.ogorodnikov1@shool.nsu.ru>
2024-10-07 22:14:28 +0700

    Cделал задачу 8, задачу 7 все еще не могу решить

--- task7.py
+++ task7.py
@@ -1,8 +1,13 @@
 count = 0
+a = 0
 N = int(input())
 for i in range(2,N):
-    for j in range(2,i):
+    for j in range(2,i+1):
         if i%j==0: count +=1
-        if count>1:break
-    print(i)
+        if count>1:
+            a = j
+            break
+
+    print(a)
+
 
